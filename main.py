aa449e0d33a8253cf116f74f21cd89d906cb1408
ThatGirl03 <kwaneleayandamazibuko@gmail.com>
2024-11-08 21:13:40 +0200

    both

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=5000, debug=True)
+def handler(event, context):
+    return app(event, context)
 
     
